78c2da8219d72f6c58134b50f8461ee83796c004
Achyut Timsina <actimsina@gmail.com>
2018-07-18 08:17:07 +0545

    incomplete abstract factory

---
+++ abstract_factory/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace abstract_factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        
+        }
+    }
+}
