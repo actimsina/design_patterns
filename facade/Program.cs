8780c1eb035efce6bf6892b1c1c9fcb9bf35f06a
Achyut Timsina <actimsina@gmail.com>
2019-02-04 08:28:45 +0545

    simple facade

---
+++ facade/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace facade
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      Facade f = new Facade();
+      f.MethodA();
+      f.MethodB();
+    }
+  }
+}
---
+++ facade/SubSystems.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace facade
+{
+  public class SubSystemOne
+  {
+    public void MethodOne()
+    {
+      Console.WriteLine("SubSystemOne Method");
+    }
+  }
+
+  public class SubSystemTwo
+  {
+    public void MethodTwo()
+    {
+      Console.WriteLine("SubSystemTwo Method");
+    }
+  }
+
+  public class SubSystemThree
+  {
+    public void MethodThree()
+    {
+      Console.WriteLine("SubsystemThree Method");
+    }
+  }
+
+  public class SubSystemFour
+  {
+    public void MethodFour()
+    {
+      Console.WriteLine("SubsystemFour Method");
+    }
+  }
+}
